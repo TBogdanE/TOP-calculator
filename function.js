68db7de14ec21145830ffc72aed895cd4b2f55ed
Toma Bogdan <bogdantoma2000@gmail.com>
2023-04-13 16:15:28 +0300

    organized the code, added comments and added the option to add negative numbers(still have some bugs)

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+//getting the DOM elements
 let displaySct1 = document.querySelector('#calculator-display-section-1');
 let displaySct2 = document.querySelector('#calculator-display-section-2');
 let key = document.querySelectorAll('.number');
@@ -5,71 +6,134 @@ let operatorBtn = document.querySelectorAll('.operator');
 const equal = document.querySelector('#equal');
 const decimal = document.querySelector('#decimal');
 const clear = document.querySelector('#clear');
+
+//initialize variables
 let currentNum = '';
 let currentOp = '';
 let previousNum = '';
 let operator = '';
-let result = 0;
+let result = '';
 
-clear.addEventListener('click', () => {
-    currentNum = '';
-    displaySct1.textContent = '';
-    displaySct2.textContent = '';
-    previousNum = '';
-    currentOp = '';
-    operator = '';
-});
 
+//EVENT LISTENERS
+
+clear.addEventListener('click', clearCalculator);
+decimal.addEventListener('click', addDecimal);
+equal.addEventListener('click', calculate);
+
+//getting number values from the keyboard
 key.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         getNumber(e.target.textContent);
     });
 });
 
-function getNumber(number) {
-    currentNum += number;
-    displaySct2.textContent = previousNum + operator + currentNum;
-    console.log(number);
-};
-
+//getting the operator btn
 operatorBtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         getOperator(e.target.textContent);
     });
 });
 
-function getOperator(op) {
-    operator = op;
-    previousNum = currentNum;
-    displaySct2.textContent = previousNum + operator;
-    currentNum = '';
-};
-
-function calculate(num1, sign, num2) {
-    num1 = Number(num1);
-    num2 = Number(num2);
-    if (sign == '+') {
-        console.log('merge');
-    } else {
-        console.log(`nu merge ${sign}`);
-    };
-    /*switch (sign) {
-        case '+':
-            result = num1 + num2;
-            console.log(result);
-            break;
-            case '-': 
-            result = num1 - num2;
-            console.log(result);
-            break;
-        default:
-            console.log('Error: operator not declared');
-            break;
-    }*/
-
-};
-
 equal.addEventListener('click', () => {
     console.log(`Previous number: ${previousNum}\n Current number: ${currentNum}\n Operator: ${operator}`);
     calculate(previousNum, operator, currentNum);
+    console.log(`The result is: ${result}`);
+    currentNum = result;
 });
+
+//FUNCTIONS
+
+//adding event to the decimal button and set it to disable after 
+//pressing it once, so you can't add more than one decimal per number
+function addDecimal() {
+    currentNum += '.';
+    displaySct2.textContent = currentNum;
+    decimal.disabled = true;
+};
+
+//clear the display when clicking 'C' button
+function clearCalculator() {
+    currentNum = '';
+    displaySct1.textContent = '';
+    displaySct2.textContent = '';
+    previousNum = '';
+    currentOp = '';
+    operator = '';
+    result = '';
+    decimal.disabled = false;
+};
+
+//setting the nummbers to variables and print them to the screen
+function getNumber(number) {
+    currentNum += Number(number);
+    displaySct2.textContent = previousNum + operator + currentNum;
+    console.log(number);
+};
+
+//setting the operators to variables
+function getOperator(op) {
+    operator = op.trim();
+    if (currentNum == '') {      //add the option to insert negative numbers
+        currentNum += operator;
+        displaySct2.textContent = currentNum;
+    } else {
+        previousNum = currentNum;
+        displaySct2.textContent = previousNum + operator;
+        currentNum = '';
+        decimal.disabled = false;
+    };
+};
+
+//check if user try to divide by 0
+function evaluate0() {
+    if (currentNum == 0 || previousNum == 0 && operator == '/') {
+        displaySct2.textContent = 'You can\'t devide by 0';
+        displaySct1.textContent = 'ERROR!';
+        throw new Error('You can\'t divide by 0');
+    };
+};
+
+//doing the operations
+function calculate(num1, sign, num2) {
+    try {
+        evaluate0();
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        switch (sign) {
+            case '+':
+                result = (num1 + num2);
+                break;
+            case '-':
+                result = (num1 - num2);
+                break;
+            case 'x':
+                result = (num1 * num2);
+                break;
+            case '/':
+                result = (num1 / num2);
+                break;
+            case '%':
+                result = (num1 % num2);
+                break;
+            default:
+                displaySct2.textContent = 'There was a problem';
+                break;
+        }
+        //checks if the numbers is a float, so it can print the decimals
+        if (result % 1 !== 0) {
+            result = result.toFixed(2);
+        };
+        displaySct1.textContent = displaySct2.textContent;
+        displaySct2.textContent = result;
+    } catch (error) {
+        console.log(error.message);
+        return;
+    }
+}
+
+/*
+- dupa ce calculez, si apas o tasta, sa se stearga tot de pe ecran sau rezultatul sa devina previous number;
+- sa afiseze erori daca 0/0
+- daca am apasat semnul unei operatii, iar apoi noi schimbam semnul
+*/
